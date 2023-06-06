b038c501148407e5092bd9e4e680a388c6bda54e
Ankush523 <ankush.ad050203@gmail.com>
2023-06-06 13:36:00 +0530

    api key added

--- pages/FluxPage.tsx
+++ pages/FluxPage.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import crypto from 'crypto';
+
 interface Contract {
     deployedAddress: string;
     contractAbi: string;
     deploymentChain: string;
+    apiKey: string;
 }
   
 interface AddContractProps {
@@ -16,15 +19,21 @@ function AddContract({ onContractAdded }: AddContractProps) {
     const [deployedAddress, setDeployedAddress] = useState('');
     const [contractAbi, setContractAbi] = useState('');
     const [deploymentChain, setDeploymentChain] = useState('');
-    
+    const [showPopup, setShowPopup] = useState(false);
+    const [apiKey, setApiKey] = useState('');
+
     const handleClick = () => {
-        const contract: Contract = { deployedAddress, contractAbi, deploymentChain };
+        const newApiKey = crypto.randomBytes(20).toString('hex'); // generate API Key
+        setApiKey(newApiKey);
+        const contract: Contract = { deployedAddress, contractAbi, deploymentChain, apiKey: newApiKey };
         onContractAdded(contract);
         setDeployedAddress('');
         setContractAbi('');
         setDeploymentChain('');
+        setShowPopup(true);
+        setTimeout(() => setShowPopup(false), 5000); // hide the popup after 5 seconds
     };
-    
+
     return (
         <div className='flex flex-col'>
             <label className='text-3xl mb-[20px]'>Add Contract Details</label>
@@ -35,9 +44,10 @@ function AddContract({ onContractAdded }: AddContractProps) {
             <label className='text-md mb-[5px]'>Deployment Chain</label>
             <input type="text" onChange={e => setDeploymentChain(e.target.value)} placeholder='chain' className='rounded-md h-[40px] bg-white mb-[50px] text-black'/>
             <button className='rounded-md bg-white text-black w-[fit-content] mx-[46%] px-[4%] py-[8px]' onClick={handleClick}>Add</button>
+            {showPopup && <div className='fixed top-20 right-5 bg-gray-500 text-white px-4 py-2 rounded'>Your API Key is: {apiKey}</div>}
         </div>
-      );
-    }
+    );
+}
     
     
 function App() {
@@ -59,45 +69,47 @@ function App() {
         return <AddContract onContractAdded={handleContractAdded} />;
         case 'Contracts':
             return (
-              <div className="flex flex-col">
+            <div className="flex flex-col">
                 <label className="text-3xl mb-[30px]">My Contracts</label>
                 <div className="flex mb-4">
-                  <div className="w-1/2 text-xl">Address</div>
-                  <div className="w-1/2 text-xl">Chain</div>
+                <div className="w-1/4 text-xl">Address</div>
+                <div className="w-1/4 text-xl">Chain</div>
+                <div className="w-1/2 text-xl">API Key</div>
                 </div>
                 {contracts.map((contract, index) => (
-                  <div key={index} className="flex mb-1 py-[5px]">
-                    <div className="w-1/2">{contract.deployedAddress}</div>
-                    <div className="w-1/2">{contract.deploymentChain}</div>
-                  </div>
+                <div key={index} className="flex mb-1 py-[5px]">
+                    <div className="w-1/4">{contract.deployedAddress.substring(0, 8) + "..." + contract.deployedAddress.substring(34,42)}</div>
+                    <div className="w-1/4">{contract.deploymentChain}</div>
+                    <div className="w-1/2">{contract.apiKey}</div>
+                </div>
                 ))}
-              </div>
-            )          
-        case 'Decode':
-      return (
-        <div className='flex flex-col'>
-          <label className='text-3xl mb-[10px]'>Decode Functions</label>
-          <label className='text-md mb-[10px]'>Select Contract</label>
-          <div  className='flex flex-row space-x-10 mb-[40px]'>
-            {contracts.map((contract, index) => (
-                <button key={index} onClick={() => handleAddressClick(contract)} className='bg-white text-black p-[4px] rounded-md'>
-                    {contract.deployedAddress}
-                </button>
-            ))}
-          </div>
-          <label className='mb-[10px]'>Available functions in {selectedContract?.deployedAddress} :</label>
-          {selectedContract && (
-            <div>
-              {JSON.parse(selectedContract.contractAbi)
-                .filter((entry : any) => entry.type === 'function')
-                .map((func : any, index : any) => (
-                  <p className='bg-gray-700 mb-[5px] w-[fit-content] p-[2px] rounded-md' key={index}>{`${func.name}(${func.inputs.map((input : any) => input.type).join(', ')})`}</p>
-                ))
-              }
             </div>
-          )}
-        </div>
-      );
+            )
+        case 'Decode':
+            return (
+                <div className='flex flex-col'>
+                <label className='text-3xl mb-[10px]'>Decode Functions</label>
+                <label className='text-md mb-[10px]'>Select Contract</label>
+                <div  className='flex flex-row space-x-10 mb-[40px]'>
+                    {contracts.map((contract, index) => (
+                        <button key={index} onClick={() => handleAddressClick(contract)} className='bg-white text-black p-[4px] rounded-md'>
+                            {contract.deployedAddress}
+                        </button>
+                    ))}
+                </div>
+                <label className='mb-[10px]'>Available functions in {selectedContract?.deployedAddress} :</label>
+                {selectedContract && (
+                    <div>
+                    {JSON.parse(selectedContract.contractAbi)
+                        .filter((entry : any) => entry.type === 'function')
+                        .map((func : any, index : any) => (
+                        <p className='bg-gray-700 mb-[5px] w-[fit-content] p-[2px] rounded-md' key={index}>{`${func.name}(${func.inputs.map((input : any) => input.type).join(', ')})`}</p>
+                        ))
+                    }
+                    </div>
+                )}
+                </div>
+            );
 
       default:
         return null;
